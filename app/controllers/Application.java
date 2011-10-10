3e190ebfe118fbfe5e523e68c71bb213a2cac175
kensei <kensei.kitahara@gmail.com>
2011-10-10 17:01:08 +0900

    simple tweet app

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,26 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Tweet;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		List tweets = Tweet.findLatest();
+		render(tweets);
+	}
 
+	public static void create(String msg) {
+		Tweet tweet = new Tweet();
+		tweet.tweet = msg;
+		tweet.save();
+		render(tweet);
+	}
+
+	public static void tweets() {
+		List tweets = Tweet.findLatest();
+		renderJSON(tweets);
+	}
 }
